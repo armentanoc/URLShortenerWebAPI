651f7be517544c5bb596e5bcc3358d10df343767
Carolina Armentano <armentanocarolina@gmail.com>
2024-03-01 10:44:43 -0300

    feat: TDD GREEN; logging middleware; TDD REFACTOR; turning exception middleware into filter applied globally.

---
+++ URLShortener.Tests/WebAPI/ExceptionFilterTest.cs
@@ -0,0 +1,37 @@
+﻿
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System.Net;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Routing;
+
+namespace URLShortener.Tests.WebAPI
+{
+    public class ExceptionFilterTest
+    {
+        [Fact]
+        public async Task Invoke_ShouldReturnInternalServerError()
+        {
+            // Arrange
+            var iLoggerMock = new Mock<ILogger<ExceptionFilter>>();
+            var filter = new ExceptionFilter(iLoggerMock.Object);
+            var context = new DefaultHttpContext();
+            context.Response.Body = new MemoryStream();
+
+            var exceptionContext = new ExceptionContext(new ActionContext(context, new RouteData(), new ActionDescriptor()), new List<IFilterMetadata>())
+            {
+                Exception = new Exception("Test exception"),
+            };
+
+            // Act
+            await filter.OnExceptionAsync(exceptionContext);
+
+            // Assert
+            context.Response.StatusCode.Should().Be((int)HttpStatusCode.InternalServerError);
+        }
+    }
+}
---
+++ URLShortener.Tests/WebAPI/LoggingMiddlewareTest.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Moq;
+using URLShortener.WebAPI.Middlewares;
+
+namespace URLShortener.Tests.WebAPI
+{
+    public class LoggingMiddlewareTest
+    {
+        [Fact]
+        public async Task InvokeAsync_ShouldLogTwiceForBeginAndEndOfRequest()
+        {
+            // Arrange
+            var EXPECTED_TIMES_TO_LOG = 2;
+            var iLoggerMock = new Mock<ILogger<LoggingMiddleware>>();
+            var middleware = new LoggingMiddleware(next: (context) => Task.CompletedTask, iLoggerMock.Object);
+            var context = new DefaultHttpContext();
+
+            // Act
+            await middleware.InvokeAsync(context);
+
+            // Assert
+            iLoggerMock.Verify(
+                x => x.Log(
+                    It.IsAny<LogLevel>(),
+                    It.IsAny<EventId>(),
+                    It.IsAny<It.IsAnyType>(),
+                    It.IsAny<Exception>(),
+                    It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+                    Times.Exactly(EXPECTED_TIMES_TO_LOG)); 
+        }
+    }
+}
+
