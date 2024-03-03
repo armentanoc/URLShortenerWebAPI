fbbb6bb0053badf5936cc6ee70dd2bb1174015ac
Carolina Armentano <armentanocarolina@gmail.com>
2024-03-03 12:03:35 -0300

    feat: new database.

---
+++ src/URLShortener.Infra/Migrations/20240303150319_CreateDatabaseInitial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace URLShortener.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDatabaseInitial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Url",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    OriginalUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    ShortenedUrl = table.Column<string>(type: "TEXT", nullable: false),
+                    ExpirationDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Url", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Url");
+        }
+    }
+}
